95d576ecda901fa712dd765a1e56af12bc4739c6
hpuranik <harshapuranik1704@gmail.com>
2025-12-26 00:03:19 -0500

    fix: token expiration timezone issue

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -25,9 +25,9 @@ def get_password_hash(password):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.now() + expires_delta
+        expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.now() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=15)
 
     to_encode.update({"exp": expire})
     jwt_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
